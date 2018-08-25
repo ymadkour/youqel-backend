8ccbaa236aa9674181b92b2bb6da67371b231a61
Youssef Madkour <youssef.madkour@digatus.de>
2018-08-25 20:28:54 +0200

    fixed flyway

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,17 @@
-FROM java:8
+# Start with a base image containing Java runtime
+FROM openjdk:8-jdk-alpine
+
+# Make port 30002 available to the world outside this container
+EXPOSE 30002
+
 VOLUME /tmp
-ADD /target/youqel-0.0.1.jar app.jar
-RUN bash -c 'touch /app.jar'
-ENTRYPOINT ["java","-jar","/app.jar"]
+
+# The application's jar file
+ARG JAR_FILE=target/youqel-0.0.1.jar
+
+# Add the application's jar to the container
+ADD ${JAR_FILE} youqel.jar
+
+# Run the jar file 
+ENTRYPOINT ["java","-Dfile.encoding=UTF-8","-jar","/youqel.jar"]
+
--- src/main/java/com/youqel/server/YouqelApplication.java
+++ src/main/java/com/youqel/server/YouqelApplication.java
@@ -1,15 +1,12 @@
 package com.youqel.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@Configuration
-@ImportResource({"classpath*:applicationContext.xml"})
+@ImportResource({ "classpath:/META-INF/spring/applicationContext.xml",
+		"classpath:/META-INF/spring/database-flyway-migration.xml", "classpath:/META-INF/spring/database-props-createddl.xml" })
 public class YouqelApplication {
 
 	public static void main(String[] args) {
